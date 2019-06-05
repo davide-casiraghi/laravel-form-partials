37c8d2c7515a236b33f300dd73d9995144282695
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-06-05 08:11:30 +0200

    added language files and fixed bug with the upload image input hidden field

--- resources/views/buttons-back-submit.blade.php
+++ resources/views/buttons-back-submit.blade.php
@@ -1,9 +1,9 @@
 {{-- Submit --}}
-    <button type="submit" class="btn btn-primary float-right">@lang('laravel-smart-blog::general.submit')</button>
+    <button type="submit" class="btn btn-primary float-right">@lang('laravel-form-partials::general.submit')</button>
 
 {{-- Back --}}
     @if(empty($routeParameter))
-        <a class="btn btn-outline-primary mr-2 float-right" href="{{ route($route) }}">@lang('laravel-smart-blog::general.back')</a>
+        <a class="btn btn-outline-primary mr-2 float-right" href="{{ route($route) }}">@lang('laravel-form-partials::general.back')</a>
     @else
-        <a class="btn btn-outline-primary mr-2 float-right" href="{{ route($route, $routeParameter) }}">@lang('laravel-smart-blog::general.back')</a>
+        <a class="btn btn-outline-primary mr-2 float-right" href="{{ route($route, $routeParameter) }}">@lang('laravel-form-partials::general.back')</a>
     @endif   
