b0f4dbf13cb6a3ab6dfea30ea938d7bf9be8f1d8
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-06-08 11:53:29 +0200

    Apply fixes from StyleCI (#1)

--- tests/LaravelFormPartialsTest.php
+++ tests/LaravelFormPartialsTest.php
@@ -2,25 +2,24 @@
 
 namespace DavideCasiraghi\LaravelFormPartials\Tests;
 
-use DavideCasiraghi\LaravelFormPartials\Facades\LaravelFormPartials;
 use Illuminate\Support\Facades\Storage;
+use DavideCasiraghi\LaravelFormPartials\Facades\LaravelFormPartials;
 
 class LaravelFormPartialsTest extends TestCase
 {
-
     /** @test */
-        public function it_uploads_an_image()
-        {
-            $imageSubdir = 'test_subdir';
-            
-            // Delete directory
-            //dd(Storage::directories('public/images')); // List directories
-            $directory = 'public/images/'.$imageSubdir.'/';
-            Storage::deleteDirectory($directory);
+    public function it_uploads_an_image()
+    {
+        $imageSubdir = 'test_subdir';
 
-            // Symulate the upload
-            $local_test_file = __DIR__.'/test-files/large-avatar.png';
-            $uploadedFile = new \Illuminate\Http\UploadedFile(
+        // Delete directory
+        //dd(Storage::directories('public/images')); // List directories
+        $directory = 'public/images/'.$imageSubdir.'/';
+        Storage::deleteDirectory($directory);
+
+        // Symulate the upload
+        $local_test_file = __DIR__.'/test-files/large-avatar.png';
+        $uploadedFile = new \Illuminate\Http\UploadedFile(
                     $local_test_file,
                     'large-avatar.png',
                     'image/png',
@@ -29,21 +28,21 @@ class LaravelFormPartialsTest extends TestCase
                     true
                 );
 
-            // Call the function uploadImageOnServer()
-            $imageFile = $uploadedFile;
-            $imageName = $imageFile->hashName();
-            $imageWidth = '1067';
-            $thumbWidth = '690';
+        // Call the function uploadImageOnServer()
+        $imageFile = $uploadedFile;
+        $imageName = $imageFile->hashName();
+        $imageWidth = '1067';
+        $thumbWidth = '690';
 
-            LaravelFormPartials::uploadImageOnServer($imageFile, $imageName, $imageSubdir, $imageWidth, $thumbWidth);
+        LaravelFormPartials::uploadImageOnServer($imageFile, $imageName, $imageSubdir, $imageWidth, $thumbWidth);
 
-            // Leave this lines here - they can be very useful for new tests
-            //$directory = "/";
-            //dump(Storage::allDirectories($directory));
-            //dd(Storage::allFiles($directory));
+        // Leave this lines here - they can be very useful for new tests
+        //$directory = "/";
+        //dump(Storage::allDirectories($directory));
+        //dd(Storage::allFiles($directory));
 
-            $filePath = 'public/images/'.$imageSubdir.'/'.$imageName;
+        $filePath = 'public/images/'.$imageSubdir.'/'.$imageName;
 
-            Storage::assertExists($filePath);
-        }    
+        Storage::assertExists($filePath);
+    }
 }
